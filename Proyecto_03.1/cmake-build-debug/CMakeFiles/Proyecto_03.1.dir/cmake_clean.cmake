1189685cdbf102a812681242f0bf19128da42c76
alberto-julian <39810145+alberto-julian@users.noreply.github.com>
2020-11-15 12:33:57 -0500

    Add files via upload

---
+++ Proyecto_03.1/cmake-build-debug/CMakeFiles/Proyecto_03.1.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Proyecto_03.1.dir/MyWindow.cpp.obj"
+  "CMakeFiles/Proyecto_03.1.dir/Particula.cpp.obj"
+  "CMakeFiles/Proyecto_03.1.dir/main.cpp.obj"
+  "Proyecto_03.1.exe"
+  "Proyecto_03.1.exe.manifest"
+  "Proyecto_03.1.pdb"
+  "libProyecto_03.1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Proyecto_03.1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
